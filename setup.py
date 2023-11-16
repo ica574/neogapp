93c68dfb2b8e63e757ca873a62ccab3e86f730b7
Isaac Cilia Attard <iciliaat@gmail.com>
2023-11-16 10:06:14 +0100

    Added dependencies
    Specified package dependencies in 'setup.py'.

--- setup.py
+++ setup.py
@@ -6,10 +6,18 @@ setup(
     name="neogapp",
     version="1.0",
     description="A modernised version of the Gaussian Processes in Python package to be used natively with Python 3",
+    license="GPLv3",
     author="Isaac Cilia Attard",
     author_email="contact@isaacciliaattard.com",
     original_author="Marina Seikel",
     original_author_email="marina@jorrit.de",
     url="http://www.acgc.uct.ac.za/~seikel/GAPP/index.html",
     packages=["gapp", "gapp.covfunctions"],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'emcee',
+        'acor',
+    ]
 )
